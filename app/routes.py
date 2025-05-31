f7a2a4231ccdae4022bdf1f65c04ec6e8ac040d3
bzhan <bzhang5460@gmail.com>
2025-05-31 17:32:20 -0400

    e

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template, url_for, redirect, flash, request
 from app import app
+
 @app.route("/")
 def home():
-    return
+    return redirect(url_for('main'))
+
 @app.route("/main")
 def main():
+    # List of assets needed for the moon landing page
+    # These could be checked/created if not existing
+    required_assets = [
+        'earth.png',
+        'lunar-module.png',
+        'astronaut.png',
+        'american-flag.png',
+        'earth-from-moon.png'
+    ]
+
     return render_template("main.html")
