480331a82cd6476e6e50a447fc8af4008aee5ac6
bzhan <bzhang5460@gmail.com>
2025-06-01 23:49:02 -0400

    e

--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,4 @@ def home():
 
 @app.route("/main")
 def main():
-    # List of assets needed for the moon landing page
-    # These could be checked/created if not existing
-    required_assets = [
-        'earth.png',
-        'lunar-module.png',
-        'astronaut.png',
-        'american-flag.png',
-        'earth-from-moon.png'
-    ]
-
     return render_template("main.html")
